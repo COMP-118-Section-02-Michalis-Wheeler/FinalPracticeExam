cb08d4c728868f05f49f625e22bee47b7be66926
Michalis Wheeler <wheeler.m@live.unic.ac.cy>
2024-05-15 09:55:11 +0300

    Completed the allPositive

--- FinalPracticeExam/main.cpp
+++ FinalPracticeExam/main.cpp
@@ -1,14 +1,66 @@
-//
-//  main.cpp
-//  FinalPractiseExam
-//
-//  Created by Michael Wheeler on 15/05/2024.
-//
+/**\file FinalPracticeExam
+ * \brief Small math program...
+ *  \author Wheeler
+ *   \date 15/05/2024
+ */
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+ // Prototypes
+bool allPositive(double arr[], int maxSize);
+
+
+/**
+ *The driver of the program
+ * @return Returns 0.
+ */
+int main() {
+    int choice;
+    
+    do {
+        cout << "\n1) Sum of range";
+        cout << "\n2) Product of range";
+        cout << "\n3) All positive";
+        cout << "\n4) Exit";
+        cout << "\n5) Enter choice: ";
+        cin >> choice;
+        
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4: cout << "\nHope to see you again!" << endl;
+                break;
+            default:
+                cerr << "\nWrong choice!";
+                break;
+        }
+    } while (choice != 4);
+    
     return 0;
 }
+/**
+ * Returns true if all values in array are 0 and greater.
+ *  @param arr The array containing the values
+ *  @param maxSize Length of the array
+ *  @return Returns <code>true</code> if all values are 0 or greater
+ *       <code>false</code> otherwise.
+ */
+bool allPositive(double arr[], int maxSize) {
+    bool ret = true;
+    int idx = 0;
+    
+    while (idx < maxSize && ret == true) {
+        if (arr[idx] < 0)
+            ret = false;
+        ++idx;
+    }
+    return ret;
+    }
+    
+
