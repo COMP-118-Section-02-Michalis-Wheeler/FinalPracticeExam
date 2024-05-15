483a2c80b9519c2ccc0899a50979eadd5186f44f
Michalis Wheeler <wheeler.m@live.unic.ac.cy>
2024-05-15 11:37:10 +0300

    test 2

--- FinalPracticeExam/UnitTestFinal.cpp
+++ FinalPracticeExam/UnitTestFinal.cpp
@@ -6,3 +6,20 @@
 //
 
 #include "UnitTestFinal.hpp"
+using namespace std;
+
+public:
+// Testing allPositive
+TEST_METHOD(TestMethod_AllPositive) {
+    // Arrange
+    const int SIZE = 5;
+    double arr[SIZE] = { 1.3, 2.2, -3.5, 0.0, 10.2 };
+    bool res;
+    // Act
+    res = allPositive(arr, SIZE);
+    
+    // Assert
+    Assert::IsFalse(res);
+    // test
+    
+}
